346f87493cbf6e61204f4deb5d421f3a42ce9270
dsadara <lim971230@gmail.com>
2023-01-06 18:43:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/dsadara/aptApp/AptAppApplication.java
@@ -0,0 +1,13 @@
+package com.dsadara.aptApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptAppApplication.class, args);
+	}
+
+}
