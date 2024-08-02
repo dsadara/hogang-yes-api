fd0a9c9a02d0176849f96701c17176831feda8fa
dsadara <chaehyun.forest@gmail.com>
2024-08-02 18:23:47 +0900

    feat: 배포 스크립트 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/github-action
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=hogang-yes
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull"
+
+git pull
+
+echo "> 프로젝트 Build 시작"
+
+./gradlew build
+
+echo "app 디렉토리로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f aptApp)
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
-echo "$CURRENT_PID"
+echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -18,16 +37,8 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/*.jar $REPOSITORY/jar
-
-JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'aptApp' | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/jar/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
