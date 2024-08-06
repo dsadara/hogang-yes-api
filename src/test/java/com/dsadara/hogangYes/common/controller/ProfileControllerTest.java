6d1adf9cac9576c79cd332889b260c7f3917be4a
dsadara <chaehyun.forest@gmail.com>
2024-08-06 09:47:43 +0900

    feat: 현재 프로파일을 반환하는 API 구현

---
+++ src/test/java/com/dsadara/hogangYes/common/controller/ProfileControllerTest.java
@@ -0,0 +1,60 @@
+package com.dsadara.hogangYes.common.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProfileControllerTest {
+
+    @Test
+    public void prod_profile이_조회된다() throws Exception {
+        // given
+        String expectedProfile = "prod";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile("dev");
+        env.addActiveProfile("local");
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertEquals(expectedProfile, profile);
+    }
+
+    @Test
+    public void prod_profile이_없으면_첫_번째가_조회된다() throws Exception {
+        // given
+        String expectedProfile = "dev";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("local");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertEquals(expectedProfile, profile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() throws Exception {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertEquals(expectedProfile, profile);
+    }
+
+}
